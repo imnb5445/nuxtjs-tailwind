0be30477b1f53c2fd9491673e8e0d0dab1ca3084
wayanAri <iwayanariutra13@gmail.com>
2024-10-03 13:36:50 +0800

    added an insertepisode function

--- pages/insert.vue
+++ pages/insert.vue
@@ -4,20 +4,7 @@
         <input type="text" v-model="sinopsis_input">
        <input type="file" accept="image/png, image/jpeg" @change="handleFileChange">
        <img v-if="imagePreview" :src="imagePreview" alt="Image Preview" class="w-20 h-20 bg-cover"/>
-        <p>Genre</p>
-       
-        <div v-for="(karakter, index) in nama_karakter" :key="index">
-            <input type="text" class="input_nama_karakter" v-model="nama_karakter[index]">
-            <input type="text" class="input_nama_karakter">
-            <select name="pemain" id="selectTest"  v-model="pemain_karakter[index]">
-                <option value="" disabled selected>Choose An Actor/Actress</option>
-                <option v-for="pemain in total_pemain" v-bind:value="pemain.pemain_id" :placeholder="'Input ' + (index + 1)">{{ pemain.nama_pemain }} </option>
-            </select>
-            <select name="awda" id="awda">
-                <option value="awda">dwadawda</option>
-            </select>
-        </div>
-        
+        <p>Tempat tayang</p>
         <div v-for="tempat in tempatTayang">
             <input type="checkbox" name="checkbox" class="input_checkbox"   v-model="tempatTayangInput" :value="tempat.tempat_tayang_id">
             <label for="checkbox">{{ tempat.nama_tempat_tayang }}</label>
@@ -29,12 +16,31 @@
             <label for="checkbox">{{ genre.nama_genre }}</label>
 
         </div>
+
+        <div v-for="(karakter, index) in nama_karakter" :key="index">
+            <input type="text" class="input_nama_karakter" v-model="nama_karakter[index]">
+            <select name="pemain" id="selectTest"  v-model="pemain_karakter[index]">
+                <option value="" disabled selected>Choose An Actor/Actress</option>
+                <option v-for="pemain in total_pemain" v-bind:value="pemain.pemain_id" :placeholder="'Input ' + (index + 1)">{{ pemain.nama_pemain }} </option>
+            </select>
+        </div>
+
+        <div v-for="(episode, index) in nama_episode" :key="index">
+            <input type="text" class="input_nama_karakter" v-model="nama_episode[index]">
+            <input type="text" class="input_nama_karakter" v-model="episode_url[index]">
+            
+        </div>
         <input type="submit">
     </form>
     
+    <div>
+        <button @click="removeInputKarakter(index)" class="bg-black text-white">Remove Karakter</button>
+    <button @click="addInputKarakter" class="bg-black text-white">Add Karakter</button>
+    </div>
+    
 
-    <button @click="removeInput(index)" class="bg-black text-white">Remove</button>
-    <button @click="addInput" class="bg-black">Add Input</button>
+    <button @click="removeInputEpisode(index)" class="bg-black text-white">Remove episode</button>
+    <button @click="addInputepisode" class="bg-black text-white">Add episode</button>
     <p>{{ genresInput }}</p>
     <p>{{ tempatTayangInput }}</p>
     <p>{{ selected_img }}</p>
@@ -51,6 +57,8 @@
     const total_pemain = ref([])
     const pemain_karakter = ref([])
     const nama_karakter = ref([])
+    const nama_episode = ref([])
+    const episode_url = ref([])
     const imgurl= ref('')
     const imagePreview = ref('')
     const selected_img = ref('')
@@ -74,16 +82,34 @@
             }
     };
 
-    const addInput = () => {
+    const addInputKarakter = () => {
         nama_karakter.value.push(''); // Add a new empty string for the new input
     };
 
-    const removeInput = () => {
+    const removeInputKarakter = () => {
+        let panjang = nama_karakter.value.length
         if (nama_karakter.value.length > 0) {
         nama_karakter.value.pop(); // Remove the last character name
-        pemain_karakter.value.pop(); // Remove the last player selection
+        if (pemain_karakter.value[panjang]) {
+            pemain_karakter.value.pop(); // Remove the last player selection
+        }
+        
     }
     };
+
+    const addInputepisode = () => {
+        nama_episode.value.push(''); // Add a new empty string for the new input
+        episode_url.value.push('')
+    };
+
+    const removeInputEpisode = () => {
+        
+        if (nama_karakter.value.length > 0) {
+        nama_episode.value.pop(); // Remove the last character name
+       episode_url.value.pop()
+    }
+    };
+
 
     //Select Function
     async function FecthDataGenre() {
@@ -184,6 +210,16 @@
         }
     }
 
+    async function InsertDataEpisode(SeriesId) {
+        const jumlah_episode = nama_episode.value.length
+        for( let i = 0; i < jumlah_episode; i++){
+            const {error} = await supabase
+            .from('table_episode')
+            .insert({nama_episode: nama_episode.value[i], episode_url: episode_url.value[i], series_id: SeriesId})
+            console.log
+        }
+    }
+
     async function InsertDataSeries( x ) {
         const { data, error} = await supabase
         .from('table_series')
@@ -194,6 +230,7 @@
         await InsertDataGenre(seriesId);
         await InsertDataTempat(seriesId)
         await InsertDataKarakter(seriesId)
+        await InsertDataEpisode(seriesId)
     }
 
 
